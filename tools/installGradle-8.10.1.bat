c067214cc0442621d54313b90d20b8ab5d1517e2
ds58 <30220598+ds58@users.noreply.github.com>
2024-09-11 14:19:25 -0500

    Add Gradle install scripts for Linux, Windows (#6)

---
+++ tools/installGradle-8.10.1.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0installGradle-8.10.1.ps1"
+pause
---
+++ tools/installGradle-8.10.1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set Gradle version
+GRADLE_VERSION="8.10.1"
+
+# Set installation directory
+INSTALL_DIR="/opt/gradle"
+
+# Update package list and install required packages
+sudo apt update
+sudo apt install -y wget unzip
+
+# Download Gradle
+echo "Downloading Gradle ${GRADLE_VERSION}..."
+wget "https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip" -P /tmp
+
+# Create installation directory
+sudo mkdir -p $INSTALL_DIR
+
+# Extract Gradle
+echo "Extracting Gradle..."
+sudo unzip -d $INSTALL_DIR /tmp/gradle-${GRADLE_VERSION}-bin.zip
+
+# Create symbolic link
+sudo ln -s "${INSTALL_DIR}/gradle-${GRADLE_VERSION}" "${INSTALL_DIR}/latest"
+
+rm /usr/bin/gradle
+ln -s "${INSTALL_DIR}/latest/bin/gradle" /usr/bin/gradle
+
+# Clean up
+rm /tmp/gradle-${GRADLE_VERSION}-bin.zip
+
+echo "Gradle ${GRADLE_VERSION} has been installed."
