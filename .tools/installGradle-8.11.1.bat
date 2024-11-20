b8a220c55a284e5e88865ea1b74c19a74cda90d8
Dexton Anderson <danderson@ihmc.org>
2024-11-20 16:18:26 -0600

    Add install scripts for Gradle 8.11.1

---
+++ .tools/installGradle-8.11.1.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0installGradle-8.11.1.ps1"
+pause
--- .tools/installGradle-8.11.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0installGradle-8.11.ps1"
-pause
