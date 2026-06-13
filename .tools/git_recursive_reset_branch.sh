c4f1b0715890b06c0db5dbf56c779bad7c749aed
Duncan Calvert <dcalvert@ihmc.org>
2026-06-13 11:40:07 -0500

    Handle differing default branches.

--- .tools/git_recursive_reset_branch.sh
+++ .tools/git_recursive_reset_branch.sh
@@ -3,15 +3,15 @@
 source "$(dirname "$0")/common_git_functions.sh"
 
 git_recursive fetch --all --prune
-git_recursive checkout develop
+git_recursive checkout _default_
 git_recursive reset --hard
 git_recursive pull
 
 git_recursive_delete_merged_branches
 
-if [ "$1" != "develop" ]; then
+if [ -n "$1" ]; then
   git_recursive checkout "$1"
   git_recursive reset --hard
   git_recursive pull
-  git_recursive rebase develop
+  git_recursive rebase _default_
 fi
