2d5231838aa6cc8ca45a9e4f0504f5e51a58f5e5
Duncan Calvert <dcalvert@ihmc.org>
2026-06-13 11:41:13 -0500

    Add submodule updates.

--- .tools/git_recursive_reset_branch.sh
+++ .tools/git_recursive_reset_branch.sh
@@ -6,6 +6,7 @@ git_recursive fetch --all --prune
 git_recursive checkout _default_
 git_recursive reset --hard
 git_recursive pull
+git_recursive submodule update --init --recursive
 
 git_recursive_delete_merged_branches
 
@@ -13,5 +14,6 @@ if [ -n "$1" ]; then
   git_recursive checkout "$1"
   git_recursive reset --hard
   git_recursive pull
+  git_recursive submodule update --init --recursive
   git_recursive rebase _default_
 fi
